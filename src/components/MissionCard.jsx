c42627412fcb8959724586a347a553b1c825370d
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-21 17:00:35 -0300

    10 terminado e projeto terminado

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class MissionCard extends Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <li>
+          <p data-testid="mission-name">{ name }</p>
+          <p data-testid="mission-year">{ year }</p>
+          <p data-testid="mission-country">{ country }</p>
+          <p data-testid="mission-destination">{ destination }</p>
+        </li>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+};
+
+export default MissionCard;
